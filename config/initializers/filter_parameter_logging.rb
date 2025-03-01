1cea6a662590bc03516551a4d9ea112a9724a70d
kandalog <asuasu.tee@gmail.com>
2025-03-01 19:26:09 +0900

    change: 開発時のfilter_parameterの変更

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,6 +3,12 @@
 # Configure parameters to be partially matched (e.g. passw matches password) and filtered from the log file.
 # Use this to limit dissemination of sensitive information.
 # See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
-Rails.application.config.filter_parameters += [
-  :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :cvv, :cvc
-]
+if Rails.env.production? || Rails.env.staging?
+  Rails.application.config.filter_parameters += [
+    :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :cvv, :cvc
+  ]
+else
+  Rails.application.config.filter_parameters += [
+    :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :cvv, :cvc
+  ]
+end
