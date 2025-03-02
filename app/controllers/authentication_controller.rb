172248f530fa795a39a7fea8d68f13431b4dfdbd
kandalog <asuasu.tee@gmail.com>
2025-03-02 19:13:28 +0900

    fix: tokenのresponse形式を変更

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,7 +3,7 @@ class AuthenticationController < ApplicationController
     @user = User.find_by_email(params[:user][:email])
     if @user&.authenticate(params[:user][:password])
       token = create_token(@user.id)
-      render json: token
+      render json: { token: }
     else
       render status: :unauthorized
     end
