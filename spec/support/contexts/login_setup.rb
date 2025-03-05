00ebe1a9643e553f2af81946a8f4384c53f1687e
kandalog <asuasu.tee@gmail.com>
2025-03-05 20:05:33 +0900

    feat: ログイン処理の共通化

---
+++ spec/support/contexts/login_setup.rb
@@ -0,0 +1,9 @@
+RSpec.shared_context 'login setup' do
+  User.destroy_all
+  let(:login_user) { create(:user, password: "Password1234!") }
+  let(:login_headers) do
+    post "/login", params: { user: { email: login_user.email, password: "Password1234!" } }
+    token = body["token"]
+    { "ACCEPT" => "application/json", "Authorization" => "Bearer #{token}" }
+  end
+end
