a9321eda11a260a6f385b212a0f5d888744161f0
kandalog <asuasu.tee@gmail.com>
2025-03-08 02:26:04 +0900

    fix: user factory修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 8, mix_case: true, special_characters: true) }
+    password { "Password123!" }
     password_confirmation { password }
     is_admin { false }
   end
