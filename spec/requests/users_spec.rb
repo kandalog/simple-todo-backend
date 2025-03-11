248db68953292b0e74a3e22e6046c79fb5984ee2
kandalog <asuasu.tee@gmail.com>
2025-03-12 01:12:55 +0900

    refactor: all系でデータ挿入をしていないので初期化処理は不要

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  after { User.destroy_all }
-
   describe "GET /users" do
     context 'adminがログインしているとき' do
       include_context 'admin setup'
