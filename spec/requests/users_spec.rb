d7df4475440b12216ba9d97953d0517a04d1a1ba
kandalog <asuasu.tee@gmail.com>
2025-03-12 01:11:52 +0900

    update: users request spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,57 +4,145 @@ RSpec.describe "Users", type: :request do
   after { User.destroy_all }
 
   describe "GET /users" do
-    include_context 'admin setup'
+    context 'adminがログインしているとき' do
+      include_context 'admin setup'
 
-    before { get '/users', headers: admin_headers }
+      before { get '/users', headers: admin_headers }
 
-    it 'adminはユーザー覧を取得できる' do
-      expect(response).to be_successful
-      expect(body.size).to eq(1)
+      it 'statusが200である' do
+        expect(response).to be_successful
+      end
+
+      it 'ユーザー覧を取得できる' do
+        expect(body[:users].size).to eq(1)
+      end
+    end
+
+    context '一般ユーザーがログインしているとき' do
+      include_context 'login setup'
+
+      before { get '/users', headers: login_headers }
+
+      it 'statusが401である' do
+        expect(response).to be_unauthorized
+      end
+
+      it '適切なメッセージが返る' do
+        expect(body[:errors]).to eq('権限がありません')
+      end
+    end
+
+    context 'ログインしていないとき' do
+      before { get '/users' }
+
+      it 'statusが401である' do
+        expect(response).to be_unauthorized
+      end
+
+      it '適切なメッセージが返る' do
+        expect(body[:errors]).to eq('ログインが必要です')
+      end
     end
   end
 
   describe "GET /users/:id" do
-    include_context 'login setup'
+    context "ログインしているとき" do
+      include_context 'login setup'
 
-    before { get "/users/#{login_user.id}", headers: login_headers }
+      before { get "/users/#{login_user.id}", headers: login_headers }
 
-    it 'ユーザーを取得できる' do
-      expect(response).to be_successful
-      expect(body[:user][:id]).to eq(login_user.id)
+      it 'statusが200である' do
+        expect(response).to be_successful
+      end
+
+      it 'ユーザーを取得できる' do
+        expect(body[:user][:id]).to eq(login_user.id)
+      end
+    end
+
+    context "ログインしていないとき" do
+      before { get "/users/1" }
+
+      it 'statusが401である' do
+        expect(response).to be_unauthorized
+      end
+
+      it '適切なメッセージが返る' do
+        expect(body[:errors]).to eq('ログインが必要です')
+      end
     end
   end
 
   describe "POST /users" do
     before { post '/users', params: { user: { email: 'sample@example.com', password: "Password123!" } } }
 
-    it 'ユーザーを作成できる' do
+    it 'statusが200である' do
       expect(response).to be_successful
       expect(body[:user][:email]).to eq('sample@example.com')
+    end
+
+    it '作成したuserが返される' do
+      expect(body[:user][:email]).to eq('sample@example.com')
+    end
+
+    it 'userの合計数が1つ増えている' do
+      expect(User.count).to eq(1)
     end
   end
 
   describe "PUT /users/'id" do
-    include_context 'login setup'
+    context 'ログインしているとき' do
+      include_context 'login setup'
 
-    it 'ユーザーを更新できる' do
-      put "/users/#{login_user.id}",
-        params: { user: { email: "updated@gmail.com", password: "Password123!" } },
-        headers: login_headers
+      before { put "/users/#{login_user.id}", params: { user: { email: "updated@gmail.com", password: "Password123!" } }, headers: login_headers }
 
-      expect(response).to be_successful
-      expect(body[:user][:email]).to eq('updated@gmail.com')
+      it 'statusが200である' do
+        expect(response).to be_successful
+      end
+
+      it 'ユーザーを更新できる' do
+        expect(body[:user][:email]).to eq('updated@gmail.com')
+      end
+    end
+
+    context 'ログインしていないとき' do
+      before { put "/users/1", params: { user: { email: "updated@gmail.com", password: "Password123!" } } }
+
+      it 'statusが401である' do
+        expect(response).to be_unauthorized
+      end
+
+      it '適切なメッセージが返る' do
+        expect(body[:errors]).to eq('ログインが必要です')
+      end
     end
   end
 
   describe "DELETE /users/:id" do
-    include_context 'login setup'
+    context 'ログインしているとき' do
+      include_context 'login setup'
 
-    before { delete "/users/#{login_user.id}", headers: login_headers }
+      before { delete "/users/#{login_user.id}", headers: login_headers }
 
-    it 'ユーザーを削除できる' do
-      expect(response).to be_successful
-      expect(User.count).to eq(0)
+      it 'statusが200である' do
+        expect(response).to be_successful
+      end
+
+      it 'ユーザーの合計数が1つ減っている' do
+        expect(User.count).to eq(0)
+      end
+    end
+
+    context 'ログインしていないとき' do
+      before { delete "/users/1" }
+
+      it 'statusが401である' do
+        expect(response).to be_unauthorized
+      end
+
+      it '適切なメッセージが返る' do
+        expect(body[:errors]).to eq('ログインが必要です')
+      end
     end
   end
 end
